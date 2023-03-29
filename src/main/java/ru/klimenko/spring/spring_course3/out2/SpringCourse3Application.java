b40478fa18cf7c1d9e29528c3bd7a54bb8198a8e
Anton <axespb@gmail.com>
2023-03-29 22:03:18 +0300

    2023.03.08

---
+++ src/main/java/ru/klimenko/spring/spring_course3/out2/SpringCourse3Application.java
@@ -0,0 +1,13 @@
+package ru.klimenko.spring.spring_course3.out2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourse3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCourse3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/klimenko/spring/spring_course3/SpringCourse3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.klimenko.spring.spring_course3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourse3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
